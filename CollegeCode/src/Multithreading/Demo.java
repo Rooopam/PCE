7d11c800b6ee67b59bc25b91c80dd16862ceadfb
ASUS <ASUS@LAPTOP-9QDC830P>
2026-01-07 00:02:58 +0530

    Multithreading

---
+++ CollegeCode/src/Multithreading/Demo.java
@@ -0,0 +1,18 @@
+package Multithreading;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Thread t= Thread.currentThread();
+		t.setName("PCE");
+		t.setPriority(7);
+		System.out.println(t);
+		duty();
+
+	}
+	static void duty() {
+		Thread t= Thread.currentThread();
+		System.out.println(t);
+	}
+
+}
